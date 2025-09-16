1dd30960ddf3a26e5a1540676eb76555c3ba0ca9
Danny <ahnd6474@gmail.com>
2025-09-16 11:37:56 +0900

    Create Rect.py

---
+++ OOP/Rect.py
@@ -0,0 +1,25 @@
+class Rect:
+	def __init__(self)
+
+x1, y1, w1, h1 = map(int, input('r1? ').split()) #생성할 첫 번째 사각형 r1의 x, y, width, height 입력
+x2, y2, w2, h2 = map(int, input('r2? ').split()) #생성할 두 번째 사각형 r2의 x, y, width, height 입력
+r1 = Rect(x1, y1, w1, h1)
+r2 = Rect(x2, y2, w2, h2)
+print(f'r1: {r1}')
+print(f'r2: {r2}')
+print(f'r1과 r2는 같은 사각형인가? {r1 == r2}')
+print(f'r1과 r2가 충돌하는가? {r1.collide(r2)}')
+r3 = r2.move_ip(-10, 10)
+print(f'r3 = r2.move_ip(-10, 10) 후')
+print(f'\t-> r2: {r2}')
+print(f'\t-> r3: {r3}')
+print(f'r1과 r2가 충돌하는가? {r1.collide(r2)}')
+mx, my = map(int, input('r1 이동? ').split())
+r4 = r1.move(mx, my)
+print(f'r4 = r1.move({mx}, {my}) 후')
+print(f'\t-> r1: {r1}')
+print(f'\t-> r4: {r4}')
+print(f'r1과 r2가 충돌하는가? {r1.collide(r2)}')
+print(f'r4와 r2가 충돌하는가? {r4.collide(r2)}')
+print(f'r2가 r4를 포함하는가? {r2.contain(r4)}')
+print(f'r4가 r2를 포함하는가? {r4.contain(r2)}')
